7d8fa19b5d284dd041e0991785941ce6aaabea07
Thuy Nguyen <phuthuynguyenn@gmail.com>
2024-01-24 16:11:46 +0700

    end of project

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = express();
 // connect db
 const connectDb = require("./db/connect");
 
+// securerity
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 // router
 const auth = require("./routes/auth");
 const jobs = require("./routes/jobs");
@@ -15,6 +21,16 @@ const authUser = require("./middleware/authentication");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use(express.json());
 // extra packages
@@ -22,7 +38,7 @@ app.use(express.json());
 // routes
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/jobs", authUser, jobs);
-app.use("/api/v1/jobs/:id", jobs);
+app.use("/api/v1/jobs/:id", authUser, jobs);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,56 @@
 const Jobs = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
-const { UnauthenticatedError, BadRequestError } = require("../errors");
-const getAllJob = (req, res) => {
-  res.send("Get all job");
+const {
+  UnauthenticatedError,
+  BadRequestError,
+  NotFoundError,
+} = require("../errors");
+const getAllJob = async (req, res) => {
+  const jobs = await Jobs.find({ createdBy: req.user.userId }).sort(
+    "createdAt"
+  );
+  res.status(StatusCodes.OK).send({ jobs, count: jobs.length });
 };
-const getJob = (req, res) => {
-  res.send("Get  job");
+const getJob = async (req, res) => {
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Jobs.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`Cant find id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 const creatJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Jobs.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
-const updateJob = (req, res) => {
-  res.send("update job");
+const updateJob = async (req, res) => {
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Jobs.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body
+  );
+  if (!job) {
+    throw new NotFoundError(`Cant find id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
-const deleteJob = (req, res) => {
-  res.send("delete job");
+const deleteJob = async (req, res) => {
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Jobs.findByIdAndRemove({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`Cant find id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 module.exports = { getAllJob, getJob, creatJob, updateJob, deleteJob };
