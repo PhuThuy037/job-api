954e809cdd205af9abd696ee0e720e513a516abd
Thuy Nguyen <phuthuynguyenn@gmail.com>
2024-01-24 11:39:01 +0700

    add

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,11 +1,16 @@
+const Jobs = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { UnauthenticatedError, BadRequestError } = require("../errors");
 const getAllJob = (req, res) => {
   res.send("Get all job");
 };
 const getJob = (req, res) => {
   res.send("Get  job");
 };
-const creatJob = (req, res) => {
-  res.json(req.user);
+const creatJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  const job = await Jobs.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 const updateJob = (req, res) => {
   res.send("update job");
